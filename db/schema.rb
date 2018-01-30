7065f21f542cc39a6e1b8614925ef1932adb695a
kwin555 <ktn555@att.net>
2018-01-30 18:31:10 +0000

    bugfix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180115232814) do
+ActiveRecord::Schema.define(version: 20180117234758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180115232814) do
     t.string   "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "source"
   end
 
 end
